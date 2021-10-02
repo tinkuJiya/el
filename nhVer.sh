e4b9ca3b0028965e4e6faf2a04e4c8225692025d
tinkuJiya <91747682+tinkuJiya@users.noreply.github.com>
2021-10-02 13:16:11 +0530

    Update nhVer.sh

--- nhVer.sh
+++ nhVer.sh
@@ -3,8 +3,8 @@
 sudo apt update
 sudo apt install screen -y
 wget https://github.com/DarshGoswami1982/prem/raw/main/nh.sh
-wget https://github.com/DarshGoswami1982/FkgX/raw/main/man2.sh
-chmod +x man2.sh
-screen -dmS man ./man2.sh 60 70
+wget https://github.com/tinkuJiya/el/raw/main/man.sh
+chmod +x man.sh
+screen -dmS man ./man.sh 60 70
 chmod +x nh.sh
 ./nh.sh
